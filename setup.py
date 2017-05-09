1be967546658f57534bb457544049d4f54b42f83
Malte Böhme <malte.boehme@affiliprint.com>
2017-05-09 12:04:29 +0200

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='django-exact',
@@ -7,7 +7,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.1.1',
+	version='0.1.2',
 
 	description='API Wrapper and Django app for Exact Online',
 	# long_description='',
@@ -24,8 +24,8 @@ setup(
 
 	# You can just specify the packages manually here if your project is
 	# simple. Or you can use find_packages().
-	packages=["exact"],
-
+	packages=find_packages(),
+	include_package_data=True,
 	# Alternatively, if you want to distribute just a my_module.py, uncomment
 	# this:
 	#   py_modules=["my_module"],
@@ -34,5 +34,5 @@ setup(
 	# your project is installed. For an analysis of "install_requires" vs pip's
 	# requirements files see:
 	# https://packaging.python.org/en/latest/requirements.html
-	install_requires=['django', 'requests'],
+	install_requires=['Django', 'requests'],
 )
